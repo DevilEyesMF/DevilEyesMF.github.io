2058f22244267194631e0df33baa9e50c0ad01a2
Remko Claes <remkootje.claes@gmail.com>
2025-04-19 16:01:29 +0200

    added cookie banner

---
+++ js/cookies.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const banner = document.getElementById('cookie-banner');
+    const acceptBtn = document.getElementById('accept-cookies');
+
+    if (!localStorage.getItem('cookiesAccepted')) {
+        banner.classList.remove('d-none');
+    }
+
+    acceptBtn.addEventListener('click', () => {
+        localStorage.setItem('cookiesAccepted', 'true');
+        banner.classList.add('d-none');
+    });
+});
